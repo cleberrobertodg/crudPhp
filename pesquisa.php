e8ad64732f13dfb31b0a55e2afd8dd48634f0501
cleberrobertodg <cleberrobertodg@gmail.com>
2025-10-06 16:57:42 -0300

    adicionar página Pesquisar

--- pesquisa.php
+++ pesquisa.php
@@ -8,13 +8,80 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.rtl.min.css" integrity="sha384-CfCrinSRH2IR6a4e6fy2q6ioOX7O6Mtm1L9vRvFZ1trBncWmMePhzvafv7oIcWiW" crossorigin="anonymous">
 
-    <title>Cadastro</title>
+    <title>Pesquisar</title>
   </head>
   <body>
+
+    <?php 
+        
+            $pesquisa = $_POST['busca'] ?? '';
+        
+
+            include "conexao.php";
+
+            $sql = "SELECT * FROM pessoas WHERE nome LIKE '%$pesquisa%'";
+
+            $dados = mysqli_query($conn, $sql);
+
+            while ($linha = mysqli_fetch_assoc($dados)){
+                foreach($linha as $key => $value){
+                    echo "$key: $value<br>";
+                }
+                echo "<hr>";
+            }    
+
+    ?>
+
     <div class="container">
         <div class="row">
             <div class="col">
-                <h1>CRUD PHP</h1>
+                <h1>Pesquisar</h1>
+
+                    <nav class="navbar navbar-light bg-light">
+                        <form class="form-inline" action="pesquisa.php" method="post">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Nome" aria-label="Pesquisar" name="busca">
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+                        </form>
+                    </nav>
+
+
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                            <th scope="col">Nome completo</th>
+                            <th scope="col">Endereço</th>
+                            <th scope="col">Telefone</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Data de nascimento
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">Nome</th>
+                                <td>Rua X</td>
+                                <td>11 911111111</td>
+                                <td>@teste@teste.com.br</td>
+                                <td>07/09/1990</td>
+                                
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                                <td>Thornton</td>
+                                
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                                <td>Thornton</td>
+                                
+                            </tr>
+                        </tbody>
+                    </table>
                 
                 <a href="index.php" class="btn btn-primary">Voltar a tela inicial</a></a>
             </div>
